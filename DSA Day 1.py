ce1e581ab6988db2bae484577e7be13c5dfc0cbf
Jaid I Mulla <160870845+jaidmulla@users.noreply.github.com>
2026-01-12 18:53:38 +0530

    Add files via upload

---
+++ DSA Day 1.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        base = strs[0]
+        for i in range(len(base)):
+            for word in strs[1:]:
+                if (i == len(word) or base[i] != word[i]):
+                    return base[0:i]
+        return base
