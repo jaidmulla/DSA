4e44e61284235735cec0b6373fef5c8cdc43238a
Jaid Mulla <160870845+jaidmulla@users.noreply.github.com>
2026-01-25 20:31:44 +0530

    Add files via upload

---
+++ Find Numbers with Even Number of DigitsDay 14.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums):
+        current_sum = max_sum = nums[0]
+
+        for n in nums[1:]:
+            current_sum = max(n, current_sum + n)
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
