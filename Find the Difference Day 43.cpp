f90163f355f23e840d44e5f88d0974303135ac60
Jaid Mulla <160870845+jaidmulla@users.noreply.github.com>
2026-02-26 19:08:43 +0530

    Add files via upload

---
+++ Find the Difference Day 43.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char result = 0;
+        
+        for(char c : s) {
+            result ^= c;
+        }
+        
+        for(char c : t) {
+            result ^= c;
+        }
+        
+        return result;
+    }
+};
